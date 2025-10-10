57181e1d1108b6e0f76fe14216eae62db00d7e68
Pranav Yadav <pranav-toptal@xendit.co>
2025-10-10 11:18:52 +0530

    leaderboard link in navbar

--- app/(hero)/navbar.tsx
+++ app/(hero)/navbar.tsx
@@ -1,4 +1,5 @@
 import { AuthButton } from "@/components/auth-button";
+import { DeployButton } from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import { createClient } from "@/lib/supabase/server";
 import { hasEnvVars } from "@/lib/utils";
@@ -23,12 +24,13 @@ const Navbar = async () => {
   return (
     <nav className=" flex justify-center border-b border-b-foreground/10 h-16">
       <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-        <div className="flex gap-5 items-center font-semibold">
+        <div className="flex gap-5 items-center font-bold">
           <Link href={"/"}>Concall Alpha</Link>
-          {/* <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2">
             <DeployButton />
-          </div> */}
+          </div>
         </div>
+
         {!hasEnvVars ? (
           <EnvVarWarning />
         ) : (
