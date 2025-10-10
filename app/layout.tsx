31520d1714cb11191a99f544ef07cad505a02603
Pranav Yadav <pranav-toptal@xendit.co>
2025-10-10 13:45:41 +0530

    add app description

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const defaultUrl = process.env.VERCEL_URL
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
   title: "[beta] Concall Alpha",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  description: "extracting the best signals from concalls",
 };
 
 const geistSans = Geist({
